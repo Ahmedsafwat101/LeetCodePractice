0b99f5079745f66df2d958dff38ef570bfeee01c
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-05-06 21:42:04 +0200

    Time: 3 ms (20.43%), Space: 41.7 MB (51.77%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,25 +1,19 @@
 class Solution {
     public int specialArray(int[] nums) {
+        
 		Arrays.sort(nums);
         int start=0;
         int end=nums.length;
+        
         while(start<=end)
         {
             int mid=start+(end-start)/2;
             int curr=count(nums,mid);
-            if(curr==mid)
-            {
-                return mid;
-            }
-            else if(curr<mid)
-            {
-                end=mid-1;
-            }
-            else if(curr>mid)
-            {
-                start=mid+1;
-            }
+            if(curr==mid) return mid;
+            if(curr<mid) end=mid-1;
+            else start=mid+1;
         }
+        
         return -1;
     }
     public static int count(int[]arr,int t)
