70d8a35b462c43fab8527cb6378a0eb453f85e06
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-05-07 12:22:20 +0200

    Time: 6 ms (27.81%), Space: 43.7 MB (61.40%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        // Assume the given arrays are valid and have values
-        List<Integer> ans =  new ArrayList();
-        for(int i = 0;i<nums1.length;i++){
-            for(int j = 0;j<nums2.length;j++){
-                if(nums1[i] == nums2[j]){
-                    ans.add(nums1[i]);
-                    nums2[j] = -1;
-                    break;
-                }
-            }
-        }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+ HashMap<Integer,Integer> hashMap1 =  new HashMap();
+       List<Integer>intersection =  new ArrayList();
+          
+       for(int num: nums1){
+          hashMap1.put(num,hashMap1.getOrDefault(num,0)+1);
+       }
+          
+       for(int num:nums2){
+           if(hashMap1.containsKey(num) && hashMap1.get(num)>0){
+               intersection.add(num);
+               hashMap1.put(num,hashMap1.get(num)-1);
+           }
+       }   
+       return intersection.stream().mapToInt(i->i).toArray();   
     }
 }
