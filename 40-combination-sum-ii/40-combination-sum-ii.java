622f1d008f7676b0d13e39c189d797d646842342
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-06-04 21:27:53 +0200

    Time: 5 ms (70.10%), Space: 43.7 MB (61.93%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,27 +1,30 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        if(candidates == null || candidates.length == 0) return new ArrayList();
-        List<List<Integer>> ans = new ArrayList();
-        Arrays.sort(candidates);
-
-        backtarcking(ans,new ArrayList<Integer>(),candidates,target,0);
-        return ans;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {    
+    if(candidates  == null || candidates.length == 0) return new ArrayList();
+      List<List<Integer>> ans = new ArrayList();
+      Arrays.sort(candidates);  
+      int currSum = 0;  
+      int start = 0;  
+      helperCombination(start,candidates,ans,target,currSum, new ArrayList());  
+      return ans;  
     }
-
-
-  
-private void backtarcking(List<List<Integer>> ans,List<Integer>tempAns,int[] candidates, int target, int start){
-        if(target==0){
-            ans.add(new ArrayList(tempAns));
-            return;
+    
+    
+    private void helperCombination(int start,int[] candidates,List<List<Integer>> ans,int target, int currSum, ArrayList<Integer> subAns){
+        if(target == currSum){
+            ans.add(new ArrayList(subAns));
         }
-      
+        
         for(int i = start;i<candidates.length;i++){
-            if(i!=start && candidates[i]==candidates[i-1]) continue;
-            if(candidates[i]>target) break;
-            tempAns.add(candidates[i]);
-            backtarcking(ans,tempAns,candidates,target-candidates[i],i+1);
-            tempAns.remove(tempAns.size()-1);
+            if(i != start && candidates[i] == candidates[i-1]) continue;
+            if(currSum+candidates[i]>target) break;
+            
+            // else 
+            currSum +=candidates[i]; 
+            subAns.add(candidates[i]);
+            helperCombination(i+1,candidates,ans,target,currSum,subAns);
+            currSum-=candidates[i];
+            subAns.remove(subAns.size()-1);
         }
     }
 }
