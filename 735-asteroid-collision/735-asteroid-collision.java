b0ce2be7ab78248c430369015729eb6a485d08f0
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-06-10 18:02:58 +0200

    Time: 14 ms (35.58%), Space: 48 MB (59.56%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        if(asteroids == null || asteroids.length == 0) return new int[]{};
+        Stack<Integer> astStack = new Stack();
+        boolean canBeAdded = false;
+        for(int asteroid:asteroids){
+            if(asteroid > 0 || astStack.isEmpty()){
+                astStack.push(asteroid);
+            }else{
+                // negative 
+                while(!astStack.isEmpty()){
+                   int top = astStack.peek();
+                    if(top<0){
+                        canBeAdded = true; 
+                        break;
+                    } 
+    
+                    if(top == Math.abs(asteroid)){
+                        astStack.pop();
+                        canBeAdded = false;
+                        break;
+                    }else if(top < Math.abs(asteroid)){
+                        astStack.pop();
+                        canBeAdded = true;    
+                    }else{
+                        //destroy if equal or greater
+                        canBeAdded = false;
+                        break;
+                    }
+                }
+                if(canBeAdded)
+                    astStack.add(asteroid);
+            }
+        }
+        return astStack.stream().mapToInt(i->i).toArray();
+    }
+}
