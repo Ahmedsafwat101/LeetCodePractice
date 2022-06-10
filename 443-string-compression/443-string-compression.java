d21963d09545fb3c24f6f48f41ad4fa73d5e422f
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-06-10 16:10:03 +0200

    Time: 2 ms (54.52%), Space: 43.4 MB (69.06%) - LeetHub

--- 443-string-compression/443-string-compression.java
+++ 443-string-compression/443-string-compression.java
@@ -1,22 +1,22 @@
 class Solution {
     public int compress(char[] chars) {
+        
         if(chars == null || chars.length == 0) return 0;
         if(chars.length == 0) return 1;
+        
         StringBuilder sb = new StringBuilder();
-        int pt1 = 0;
-        int pt2 = 0;
+        int pt1 = 0, pt2 = 0;
         
        while(pt2<chars.length){
-           if(chars[pt1] ==  chars[pt2])
-               pt2++;
-           else{
-               int diff = pt2-pt1;
+           if(chars[pt1] != chars[pt2]){
+                int diff = pt2-pt1;
                sb.append(chars[pt1]);
                if(diff>1)
                    sb.append(diff);
                pt1 = pt2;
-           }   
-       }
+             }
+             pt2++;
+       }   
         
        int diff = pt2-pt1;
        sb.append(chars[pt1]);
