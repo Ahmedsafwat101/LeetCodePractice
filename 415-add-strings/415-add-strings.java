628d42f9ab4c034d62a1a9c9d9ed352c69ed3bcb
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-06-11 11:11:45 +0200

    Time: 3 ms (70.95%), Space: 42.9 MB (65.55%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -7,10 +7,10 @@ class Solution {
         int pt2 = nums2.length()-1 ;
         int cary = 0 , sum = 0, totalSum;
         StringBuilder ans = new StringBuilder();
-        while(pt1>=0 && pt2>=0){
+        while(pt1>=0 || pt2>=0){
             
-            int digit1 = nums1.charAt(pt1--)-'0';
-            int digit2 = nums2.charAt(pt2--)-'0';
+            int digit1 = pt1 >= 0 ? nums1.charAt(pt1--)-'0':0;
+            int digit2 = pt2 >= 0 ? nums2.charAt(pt2--)-'0':0;
             
             totalSum = (digit1+digit2+cary);
             sum = totalSum%10;
@@ -18,23 +18,7 @@ class Solution {
             
             ans.append(sum);
         }
-        
-        while(pt1>=0 ){
-            int digit1 = nums1.charAt(pt1--)-'0';
-            totalSum = (digit1+cary);
-            sum = totalSum%10;
-            cary= totalSum/10;
-            ans.append(sum);
-        }
-        
-        while(pt2>=0 ){
-            int digit2 = nums2.charAt(pt2--)-'0';
-            totalSum = (digit2+cary);
-            sum = totalSum%10;
-            cary= totalSum/10;
-            ans.append(sum);
-        }
-        
+            
         if(cary!=0) ans.append(cary);
         
         return ans.reverse().toString();
