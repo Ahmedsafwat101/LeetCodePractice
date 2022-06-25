7acae042f78997ad32fe4f10615c266baf78ca36
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-06-25 19:55:02 +0200

    Time: 13 ms (42.06%), Space: 47.2 MB (36.36%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,17 +1,20 @@
-class Solution {
+public class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> seenWords = new HashSet(wordDict);
-        boolean[]dp = new boolean[s.length()+1];
-        dp[0] = true;
-        
-        for(int i = 1;i<=s.length();i++){
-            for(int j = 0;j<i;j++){
-                if(dp[j] && seenWords.contains(s.substring(j,i))){
-                    dp[i] = true; // mark the start of the new word as true
-                }
+        return wordBreakMemo(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);
+    }
+
+    private boolean wordBreakMemo(String s, Set<String> wordDict, int start, Boolean[] memo) {
+        if (start == s.length()) {
+            return true;
+        }
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && wordBreakMemo(s, wordDict, end, memo)) {
+                return memo[start] = true;
             }
         }
-        return dp[s.length()];
+        return memo[start] = false;
     }
 }
-
