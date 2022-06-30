2c916c06012b0994f6af318c730f20660bd0f2f4
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-06-30 15:16:16 +0200

    Time: 16 ms (93.62%), Space: 42.1 MB (89.35%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,28 +2,17 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         if((coins == null || coins.length == 0 )&& amount > 0) return -1;
         if(amount <= 0) return 0;
-        int[]memo = new int[amount+1];
-        Arrays.fill(memo,Integer.MAX_VALUE);
-        return getMinCountRecursively(coins,amount,memo);
-    }
-    
-    private int getMinCountRecursively(int[] coins, int remain, int []memo){
-        if(remain < 0) return -1;
-        if(remain == 0) return 0;
-        if(memo[remain] != Integer.MAX_VALUE) return memo[remain];
-        
-        int minCount = Integer.MAX_VALUE;
-        
-        for(int coin: coins){
-            int count =  getMinCountRecursively(coins,remain-coin,memo);
-            if(count == -1) continue;
-            minCount = Math.min(minCount,count+1);
+        int[]dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for(int i = 1;i<=amount;i++){
+            for(int coin : coins){
+                if(coin<=i){
+                    dp[i] = Math.min(dp[i],dp[i-coin]+1);
+                }
+            }
         }
-        
-        memo[remain] =(minCount == Integer.MAX_VALUE)?-1:minCount;
-        
-        
-        return memo[remain];
-
+        return  dp[amount]>amount?-1:dp[amount];
     }
+
 }
