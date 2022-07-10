2ad22195a3d7a7c0423d8f46cee70fdc2bebb5ea
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-07-10 12:39:18 +0200

    Time: 2 ms (33.43%), Space: 44.2 MB (64.03%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+final class TreeInfo {
+  public final int height;
+  public final boolean balanced;
+
+  public TreeInfo(int height, boolean balanced) {
+    this.height = height;
+    this.balanced = balanced;
+  }
+}
+
+class Solution {
+    
+     private TreeInfo isBalancedTreeHelper(TreeNode root) {
+    // An empty tree is balanced and has height = -1
+    if (root == null) {
+      return new TreeInfo(-1, true);
+    }
+
+    // Check subtrees to see if they are balanced.
+    TreeInfo left = isBalancedTreeHelper(root.left);
+    if (!left.balanced) {
+      return new TreeInfo(-1, false);
+    }
+    TreeInfo right = isBalancedTreeHelper(root.right);
+    if (!right.balanced) {
+      return new TreeInfo(-1, false);
+    }
+
+    // Use the height obtained from the recursive calls to
+    // determine if the current node is also balanced.
+    if (Math.abs(left.height - right.height) < 2) {
+      return new TreeInfo(Math.max(left.height, right.height) + 1, true);
+    }
+    return new TreeInfo(-1, false);
+  }
+
+  public boolean isBalanced(TreeNode root) {
+         return isBalancedTreeHelper(root).balanced;
+  }
+
+}
