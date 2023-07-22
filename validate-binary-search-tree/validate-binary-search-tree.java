d4cf5a83d3569e5985cd40e89d1a2d4af74ced18
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2023-07-22 21:51:47 +0300

    Time: 0 ms, Memory: 44.4 MB - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.java
+++ validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,15 +15,16 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        Integer left  = null;
+        if(root == null) return true;
+        Integer left = null;
         Integer right = null;
         return isValidBSTHelper(root,left,right);
     }
     
-    private boolean isValidBSTHelper(TreeNode root,Integer left , Integer right){
-        if(root  == null) return true;
+    private boolean isValidBSTHelper(TreeNode root, Integer left, Integer right){
+        if(root == null) return true;
         
-        if(left!= null && left >= root.val || right!= null && right <= root.val ) return false;
-        return isValidBSTHelper(root.left,left,root.val) && isValidBSTHelper(root.right,root.val,right);
+        if(left != null && left >= root.val ||  right != null && right <= root.val ) return false;
+    return isValidBSTHelper(root.left,left,root.val) && isValidBSTHelper(root.right,root.val,right);
     }
 }
