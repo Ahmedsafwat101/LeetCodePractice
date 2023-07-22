126991bdfc08ed2a2329eb7b58747d1e42c0ef68
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2023-07-22 08:31:50 +0300

    Time: 2 ms, Memory: 45 MB - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length < 2) return;
+        int pt1 = 0 , pt2 = 0 ;
+        while(pt2 < nums.length){
+            if(nums[pt2]!=0){
+                nums[pt1] = nums[pt2];
+                if(pt1!=pt2){
+                   nums[pt2] = 0;
+                }
+                pt1++;
+            }
+            pt2++;
+        }
+    }
+}
