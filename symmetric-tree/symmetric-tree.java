2b2ff23809d64950edcf3020a36c3f184064fdbf
Ahmed Safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2023-07-23 10:39:20 +0300

    Time: 0 ms, Memory: 41.1 MB - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -16,15 +16,14 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        return isSymmetricHelper(root,root);
+        return isSymmetricHelp(root.left,root.right);
     }
-
-    private boolean isSymmetricHelper(TreeNode root1, TreeNode root2){
+    
+    private boolean isSymmetricHelp(TreeNode root1, TreeNode root2){
         if(root1 == null && root2 == null) return true;
-        if(root1 == null || root2 == null) return false;
-
-        if(root1.val != root2.val) return false;
-
-       return isSymmetricHelper(root1.left, root2.right) &&  isSymmetricHelper(root1.right,root2.left);
+        if(root1 == null || root2 == null || root1.val!=root2.val) return false;
+        
+        return isSymmetricHelp(root1.left,root2.right) && isSymmetricHelp(root1.right,root2.left);
+        
     }
 }
